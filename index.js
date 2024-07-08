072a37468d7a676a4e85433f97ce80cdbd7a9116
Frankline Were <franklinegift@gmail.com>
2024-07-08 10:12:25 +0300

    add event listener i.e DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textId = document.getElementById("text");
+
+    textId.innerHTML = "This is really cool!";
+});
